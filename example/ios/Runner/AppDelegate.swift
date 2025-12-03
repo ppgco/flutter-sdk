f032a119c74d322979ef0af9fe5795bb9006d828
amarose <adammaroszek1@gmail.com>
2025-12-03 11:42:25 +0100

    chore(ios): add example Xcode project config and update gitignore
    - Add example iOS project.pbxproj with NSE and In-App Messages config
    - Add AppDelegate.swift changes
    - Update .gitignore to exclude SPM cache directories

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -3,7 +3,7 @@ import Flutter
 import UserNotifications
 import PPG_framework
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
